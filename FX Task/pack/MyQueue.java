9fbd41467d3ea3745d1b86ee910b5072959e5a64
Fahim Shahariar <49879599+shahariar961@users.noreply.github.com>
2023-05-04 05:36:57 +0200

    FX

--- FX Task/pack/MyQueue.java
+++ FX Task/pack/MyQueue.java
@@ -4,7 +4,8 @@
 
 	import java.util.function.Consumer;
 	import java.util.Iterator;
-
+	import java.util.concurrent.ArrayBlockingQueue; //NEW		
+	;
 	// Representing a queue with some methods.
 	class MyQueue<E> {
 		
@@ -14,40 +15,35 @@
 		private int length = 6; 
 		private int first = 0;
 		private int last = length - 1;
-		private Object[] myArray;
+		private ArrayBlockingQueue<E> myArray = new ArrayBlockingQueue<>(length); // NEW
+		//private Object[] myArray;
 
 		// Inner class MyIterator.
 		class MyIterator<E> implements Iterator<E> {
-			
 			// MyIterator instance variables.
 			private int index;
-
 			// MyIterator constructor.
 			private MyIterator() {
 				index = first;
 			}	
-			
 			// MyIterator instance methods.
-			
 			public boolean hasNext() {
+
 				return index != nextIndex(last);
 			}
-			
 			@SuppressWarnings("unchecked")
 			public E next() {
-				E element = (E)myArray[index];
+				//E element = (E)myArray[index];
+				E element = (E)myArray.toArray()[index]; // NEW
 				index = nextIndex(index);
 				return element;
 			}
 		}
-		
 		// MyQueue constructor.
 		public MyQueue() {
-			myArray = new Object[length];
+			//myArray = new Object[length];
 		}
-		
 		// MyQueue instance methods.
-		
 		// Next position when regarding the array as a circle.
 		private int nextIndex(int index) {
 			if (index == length - 1)
@@ -55,54 +51,66 @@
 			else
 				return index + 1;
 		}
-		
 		// Check if the queue is empty.
 		public boolean isEmpty() {
-			return nextIndex(last) == first;
+			//return nextIndex(last) == first;
+			return myArray.isEmpty(); //NEW
 		}
-		
 		// Check if the queue is full.
 		public boolean isFull() {
-			return nextIndex(nextIndex(last)) == first;
-		}
-				
+			//return nextIndex(nextIndex(last)) == first;
+			return myArray.remainingCapacity() == 0 ; //NEW
+		}		
 		// Add an element last in the queue.
 		public boolean add(E element) {
-			if (isFull())
-				return false;
-			
+			//if (isFull())
+			//	return false;
 			// Add new element.
-			last = nextIndex(last);
-			myArray[last] = element;
+			//last = nextIndex(last);
+			//myArray[last] = element;
+			try { //NEW
+				myArray.put(element); // NEW
+			} catch (InterruptedException e) {  // NEW
+ 				return false ;  // NEW
+			}
 			return true;
 		}
-		
 		// Remove and return the first element in the queue.
 		@SuppressWarnings("unchecked")
 		public E remove() {
-			if (isEmpty())
-				return null;
-			
-			Object element = myArray[first];
-			first = nextIndex(first);
-			return (E)element;
-		}
+			//if (isEmpty())
+			try{	//NEW
+				return myArray.take(); //NEW
+			} catch (InterruptedException e) { // NEW
 
+			
+				return null;
+			}
+			//bject element = myArray[first];
+			//first = nextIndex(first);
+			//return (E)element;
+		}
 		// Perform the action for each element.
 		@SuppressWarnings("unchecked")
+		// public void forEach(Consumer<E> action) {
+		// 	int index = first;
+		// 	while (index != nextIndex(last)) {
+		// 		//action.accept((E)myArray[index]);
+		// 		action.accept(myArray.toArray((E[]) new Object[myArray.size()])[index]); // NEW
+		// 		index = nextIndex(index);
+		// 	}
+		// }
+
 		public void forEach(Consumer<E> action) {
-			int index = first;
-			while (index != nextIndex(last)) {
-				action.accept((E)myArray[index]);
-				index = nextIndex(index);
+			Iterator<E> iterator = myArray.iterator();
+			while (iterator.hasNext()) {
+				action.accept(iterator.next());
 			}
 		}
-		
 		// Returns an iterator over the elements.
 		public Iterator<E> iterator() {
 			return new MyIterator<E>();
 		}
-		
 		public static void main(String[] args) {
 			MyQueue<String> myQueue = new MyQueue<String>();
 			
@@ -111,8 +119,8 @@
 			myQueue.add("C");
 			myQueue.add("D");
 			myQueue.add("E");
-			
-			System.out.println("myQueue: ");
+			 
+ 			System.out.println("myQueue: ");
 			myQueue.forEach((x) -> System.out.println(x));
 			
 			Iterator<String> myIterator = myQueue.iterator();
---
+++ FX Task/pack/MyQueueDemo.java
@@ -0,0 +1,44 @@
+package pack;
+import java.util.Random;
+
+public class MyQueueDemo {
+    public static void main(String[] args) {
+        MyQueue<Integer> myQueue = new MyQueue<>();
+
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Random random = new Random();
+                    for (int i = 0; i < 10; i++) {
+                        if (random.nextBoolean()) {
+                            int value = random.nextInt(100);
+                            myQueue.add(value);
+                            System.out.println(Thread.currentThread().getName() + " added " + value);
+                        } else {
+                            Integer value = myQueue.remove();
+                            if (value != null) {
+                                System.out.println(Thread.currentThread().getName() + " removed " + value);
+                            }
+                        }
+                        try {
+                            Thread.sleep(random.nextInt(1000));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
